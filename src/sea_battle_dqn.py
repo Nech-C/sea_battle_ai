44ec1a1926c662142970ca708d8f18e3a1b34b96
Nech-C <xiaokunchen@outlook.com>
2023-04-23 00:04:33 -0400

    make sea_battle_dqn.py more concise and readable

--- src/sea_battle_dqn.py
+++ src/sea_battle_dqn.py
@@ -1,133 +1,141 @@
-from sea_battle import SeaBattleBoard,  SeaBattleEnvironment
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
-import gym
-from collections import deque
 import random
+import argparse
+import os
+from collections import deque
+from sea_battle import SeaBattleBoard, SeaBattleEnvironment
 
+# Constants
+BUFFER_SIZE = 10000
+NUM_SQUARES = 49
+NUM_EPISODES = 2500
+BATCH_SIZE = 2048
+GAMMA = 0.99
+EPSILON = 1.0
+EPSILON_MIN = 0.01
+EPSILON_DECAY = 0.995
+LEARNING_RATE = 0.00007
+MAX_INVALID_ACTION_COUNT = 10
+HIDDEN_LAYER_SIZE = [512, 256]
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Create ReplayBuffer class
 class ReplayBuffer:
+    """A simple replay buffer implementation to store and sample experiences."""
+
     def __init__(self, max_size):
         self.buffer = deque(maxlen=max_size)
-    
+
     def push(self, state, action, reward, next_state, done):
         self.buffer.append((state, action, reward, next_state, done))
-    
+
     def sample(self, batch_size):
         return random.sample(self.buffer, batch_size)
-    
+
     def __len__(self):
         return len(self.buffer)
-    
-# Define the DeepQNetwork class
+
+
 class DeepQNetwork(nn.Module):
-    def __init__(self, num_squares, device, hidden_layer_size=[512, 256]):
+    """A deep Q-network implementation."""
+
+    def __init__(self, num_squares, device, hidden_layer_size=HIDDEN_LAYER_SIZE):
         super(DeepQNetwork, self).__init__()
         self.fc1 = nn.Linear(num_squares, hidden_layer_size[0])
         self.fc2 = nn.Linear(hidden_layer_size[0], hidden_layer_size[1])
         self.fc3 = nn.Linear(hidden_layer_size[1], num_squares)
         self.to(device)
 
-        
     def forward(self, x):
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
         x = self.fc3(x)
         return x
-        
 
-# Define the training function
-def train(dqn, batch_size, gamma, epsilon, epsilon_min, epsilon_decay, learning_rate, device):
+
+def choose_action(state, dqn, device, epsilon, env, invalid_action_count):
+    """Choose an action based on the current state, epsilon value and DQN."""
+
+    if np.random.rand() < epsilon or invalid_action_count >= MAX_INVALID_ACTION_COUNT:
+        action = env.action_space.sample()
+    else:
+        state_tensor = torch.tensor(state, dtype=torch.float32).to(device)
+        q_values = dqn(state_tensor.unsqueeze(0))
+        action = torch.argmax(q_values).item()
+    return action
+
+
+def update_dqn(dqn, memory, optimizer, device, batch_size=BATCH_SIZE, gamma=GAMMA):
+    """Update the DQN based on a batch of experiences from the memory buffer."""
+
+    batch = memory.sample(batch_size)
+    states, actions, rewards, next_states, dones = zip(*batch)
+
+    states = torch.tensor(np.array(states), dtype=torch.float32).to(device)
+    actions = torch.tensor(actions, dtype=torch.int64).to(device)
+    rewards = torch.tensor(rewards, dtype=torch.float32).to(device)
+    next_states = torch.tensor(np.array(next_states), dtype=torch.float32).to(device)
+    dones = torch.tensor(dones, dtype=torch.bool).to(device)
+
+    current_q_values = dqn(states).gather(1, actions.unsqueeze(1)).squeeze()
+    next_q_values = dqn(next_states).max(1)[0]
+    target_q_values = rewards + gamma * next_q_values * (1 - dones.float())
+    loss = nn.MSELoss()(current_q_values, target_q_values.detach())
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    return loss.item
+def train_dqn(dqn, device, num_episodes=NUM_EPISODES, batch_size=BATCH_SIZE, gamma=GAMMA, epsilon=EPSILON, epsilon_min=EPSILON_MIN, epsilon_decay=EPSILON_DECAY, learning_rate=LEARNING_RATE):
+    """Train the DQN with the given hyperparameters."""
+
     env = SeaBattleEnvironment()
-    memory = ReplayBuffer(10000)
+    memory = ReplayBuffer(BUFFER_SIZE)
     optimizer = optim.Adam(dqn.parameters(), lr=learning_rate)
-    num_episodes = 2500
-    
-    MAX_INVALID_ACTION_COUNT = 10
-    invalid_action_count = 0
-    loss = None
+
     for episode in range(num_episodes):
-        # initialize the environment
-        state = env.reset()
-        state = state.flatten()
+        state = env.reset().flatten()
         done = False
-        steps_to_complete = 0 # the number of steps taken to complete the episode
-        
+        steps_to_complete = 0
+        invalid_action_count = 0
+        loss = None
+
         while not done:
-            if np.random.rand() < epsilon or invalid_action_count >= MAX_INVALID_ACTION_COUNT:
-                action = env.action_space.sample()
-            else:
-                q_values = dqn(torch.tensor(state, dtype=torch.float32).to(device))
-                action = torch.argmax(q_values).item()
-            
+            action = choose_action(state, dqn, device, epsilon, env, invalid_action_count)
             next_state, reward, done, _ = env.step(action=action)
             next_state = next_state.flatten()
-            
+
             if np.array_equal(state, next_state):
                 invalid_action_count += 1
             else:
                 invalid_action_count = 0
 
-            #  push the transition to the memory buffer
             memory.push(state, action, reward, next_state, done)
-            
-            # update the state and increment the step counter
             state = next_state
             steps_to_complete += 1
-            
-            # train the DQN on the memory buffer
+
             if len(memory) > batch_size:
-                batch = memory.sample(batch_size)
-                states, actions, rewards, next_states, dones = zip(*batch)
-                
-                states = torch.tensor(np.array(states), dtype=torch.float32).to(device)
-                actions = torch.tensor(actions, dtype=torch.int64).to(device)
-                rewards = torch.tensor(rewards, dtype=torch.float32).to(device)
-                next_states = torch.tensor(np.array(next_states), dtype=torch.float32).to(device)
-                dones = torch.tensor(dones, dtype=torch.bool).to(device)
-                
-                current_q_values = dqn(states).gather(1, actions.unsqueeze(1)).squeeze()
-                next_q_values = dqn(next_states).max(1)[0]
-                target_q_values = rewards + gamma * next_q_values * (1 - dones.float())
-                loss = nn.MSELoss()(current_q_values, target_q_values.detach())
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-                
+                loss = update_dqn(dqn, memory, optimizer, device)
+
         if episode % 20 == 0 and loss is not None:
-                    print(f"Episode: {episode}, Loss: {loss.item()}, Epsilon: {epsilon}, Steps to complete: {steps_to_complete}")        
-                
+            print(f"Episode: {episode}, Loss: {loss}, Epsilon: {epsilon}, Steps to complete: {steps_to_complete}")
 
-                
         if epsilon > epsilon_min:
-            epsilon *= epsilon_decay      
-            
-def main():
-    # Define hyperparameters
-    batch_size = 2048
-    gamma = 0.99
-    epsilon = 1.0
-    epsilon_min = 0.01
-    epsilon_decay = 0.995
-    learning_rate = 0.00007
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # Instantiate the DeepQNetwork class
-    num_squares = 49  # Assuming a 10x10 grid
-    dqn = DeepQNetwork(num_squares, device)
+            epsilon *= epsilon_decay
 
-    # Train the DQN
-    train(dqn, batch_size=batch_size, gamma=gamma, epsilon=epsilon, epsilon_min=epsilon_min, epsilon_decay=epsilon_decay, learning_rate=learning_rate, device=device)
 
-    torch.save(dqn.state_dict(), "trained_model.pth")
+def save_dqn(dqn, model_path="trained_model.pth"):
+    """Save the DQN model to the specified path."""
 
-    # Episode: 100, Loss: 2173297664.0, Epsilon: 0.6057704364907278
-    # Episode: 200, Loss: 57742004.0, Epsilon: 0.3669578217261671
-    # Episode: 300, Loss: 5281507.5, Epsilon: 0.22229219984074702
-    
+    torch.save(dqn.state_dict(), model_path)
+
+
+import argparse
+import os
+
 def test(dqn, device, num_test_episodes=100, max_steps=30):
     env = SeaBattleEnvironment()
     rewards = []
@@ -163,26 +171,29 @@ def test(dqn, device, num_test_episodes=100, max_steps=30):
     return average_reward, average_steps
 
 
+def main():
+    # Create an argument parser
+    parser = argparse.ArgumentParser(description="Train and test Sea Battle DQN")
+    parser.add_argument("action", choices=["train", "test"], help="Action to perform, train or test the DQN model")
+    parser.add_argument("episodes", type=int, help="Number of episodes for training or testing")
+    parser.add_argument("model_path", help="Path to the model file")
 
-def test_model(model_path, device):
-    # Instantiate the DeepQNetwork class
-    num_squares = 49  # Assuming a 10x10 grid
-    dqn = DeepQNetwork(num_squares, device,[128, 128])
+    # Parse the arguments
+    args = parser.parse_args()
 
-    # Load the trained model
-    dqn.load_state_dict(torch.load(model_path))
-    dqn.to(device)
-    dqn.eval()
-    
-    # Test the model
-    average_reward = test(dqn, device)
-    print(f"Average reward: {average_reward}")
+    # Create the DQN model
+    dqn = DeepQNetwork(NUM_SQUARES, device)
+
+    if args.action == "train":
+        train_dqn(dqn, device, num_episodes=args.episodes)
+        save_dqn(dqn, model_path=args.model_path)
+
+    elif args.action == "test":
+        dqn.load_state_dict(torch.load(args.model_path))
+        dqn.to(device)
+        dqn.eval()
+        average_reward, average_steps = test(dqn, device, num_test_episodes=args.episodes)
+        print(f"Average reward: {average_reward}, Average steps: {average_steps}")
 
 if __name__ == "__main__":
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model_path = "trained_model.pth"
-
-    # Uncomment the following line if you want to train the model again
     main()
-
-    # test_model(model_path, device)
