f83cc97526daebdc6d01c0d1fa27f6ca2f1e001d
Nech-C <xiaokunchen@outlook.com>
2023-05-12 09:31:30 -0400

    remove one_hot; update Training_instance.__init__

--- lib/utils.py
+++ lib/utils.py
@@ -4,7 +4,7 @@ import toml
 from collections import deque
 from abc import ABC, abstractmethod
 from lib.constants import CELL_MAPPING
-from sea_battle2 import GuessResult
+from src.sea_battle2 import GuessResult
 from typing import Dict
 
 
@@ -20,43 +20,18 @@ class ReplayBuffer():
         return len(self.buffer)
 
 class Training_instance(ABC):
-    def __init__(self):
-        pass
+    def __init__(self, config, device):
+        self.model_name = config["model"]["model_name"]
+        self.architecture = config["architecture"]
+        self.hyperparameters = config["hyperparameters"]
+        self.reward_function = config["reward_function"]
+        self.training = config["training"]
+        self.reward_function = load_reward_function(config["reward_function"])
+        self.device = device
+
     @abstractmethod
     def run(self):
         pass
-
-
-# def base_line(state: np.ndarray):
-#         """Returns the baseline for a given state(one-hotted)."""
-#         state = state.flatten()
-#         state = state.reshape((-1,3))
-#         hit_count = state[;,]
-        
-
-def state_to_one_hot_3(state: np.ndarray):
-    """convert a state(2d np array) to the corresponding one-hot encoding. This function should be used with env.step()
-
-    Args:
-        state (ndarray): array with integers 0, 2, 3. 0 for empty or ship, 2 for miss, 3 for hit.
-
-    Returns:
-        ndarray: the corresponding one-hot encoding
-    """
-    one_hot_state = np.zeros((*state.shape, 3), dtype=np.float32)
-
-    for i in range(state.shape[0]):
-        for j in range(state.shape[1]):
-            square = state[i, j]
-            if square == 0:  # empty and ship
-                one_hot_state[i, j, 0] = 1
-            elif square == 2:  # miss
-                one_hot_state[i, j, 1] = 1
-            elif square == 3:  # hit
-                one_hot_state[i, j, 2] = 1
-
-    return one_hot_state.flatten()
-
 
 def load_reward_function(reward_function: Dict) -> Dict[GuessResult, float]:
     # Map the keys back to the Enum members
