e2a6adfb302a27e6f0f56d92ef06e6ad02d72b59
Chen <xiaokunchen@outlook.com>
2023-05-13 16:01:30 -0400

    delete constants.py

--- lib/constants.py
+++
@@ -1,4 +0,0 @@
-CELL_MAPPING={'empty': 0, 'ship': 1, 'miss': 2, 'hit': 3}
-CELL_DISPLAY_MAPPING_INTERNAL={0: '.', 1: 'S', 2: 'M', 3: 'H'}
-CELL_DISPLAY_MAPPING_PLAYER={0: '.', 1: '.', 2: 'M', 3: 'H'}
-CELL_ONEHOT_MAPPING={0: [1,0,0], 1: [1,0,0], 2: [0,1,0], 3: [0,0,1]}
