0fc7118934a9785190ee44a768cb6b0b229e76cd
Chen <xiaokunchen@outlook.com>
2023-05-21 22:12:38 -0400

    add documentation; implement update_tensorboard()

--- lib/utils.py
+++ lib/utils.py
@@ -5,7 +5,7 @@ import torch
 import os
 from collections import deque
 from abc import ABC, abstractmethod
-from src.sea_battle import GuessResult
+from src.sea_battle import GuessResult, SeaBattleEnv
 from typing import Dict
 from torch.utils.tensorboard import SummaryWriter
 
@@ -23,7 +23,7 @@ class ReplayBuffer():
         return len(self.buffer)
 
 class Training_instance(ABC):
-    def __init__(self, config, device):
+    def __init__(self, config: dict, device):
         self.model_name = config["model"]["model_name"]
         self.architecture = config["architecture"]
         self.hyperparameters = config["hyperparameters"]
@@ -38,8 +38,33 @@ class Training_instance(ABC):
         pass
     
     def close(self, model_save_path):
+        """the method saves the model to the specified path and closes the tensorboard writer
+
+        Args:
+            model_save_path (str): the string of the save path
+        """
         torch.save(self.network, os.path.join(model_save_path, self.model_name + ".pth"))
         self.writer.close()
+    
+    def update_tensorboard(self, episode: int, env: SeaBattleEnv, loss: float, eps: float):
+        """ the method updates the tensorboard
+
+        Args:
+            episode (int): the eposode number
+            env (SeaBattleEnv): the SeaBattleEnv object
+            loss (float): policy update loss 
+            eps (float): epsilon
+        """
+        step_count, tot_guesses, effective_guesses, tot_reward = env.get_stats()
+        if loss is not None:
+            self.writer.add_scalar('Loss/train', loss, episode)
+        self.writer.add_scalar('Steps/train', step_count, episode)
+        self.writer.add_scalar('Guesses/train', tot_guesses, episode) 
+        self.writer.add_scalar('Effective guesses/train', effective_guesses, episode)
+        if eps is not None:
+            self.writer.add_scalar('Epsilon/train', eps, episode)
+        self.writer.add_scalar('reward/train', tot_reward, episode)
+        print(f"episode: {episode}\t reward:{tot_reward}")
     
 
 def load_reward_function(reward_function: Dict) -> Dict[GuessResult, float]:
