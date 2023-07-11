eeac9352cd04a7b5f550207609b506842f51f5b5
Chen <xiaokunchen@outlook.com>
2023-07-10 22:10:00 -0400

    feat: Optimize performance and enhance functionality for SeaBattleEnv

--- src/sea_battle.py
+++ src/sea_battle.py
@@ -1,14 +1,8 @@
-# sea_battle.py:
 import numpy as np
 import gym
-import numpy.random as npr
+import random as npr
 from enum import Enum
 from typing import (Any, Dict, List, Tuple)
-
-CELL_MAPPING = {'empty': 0, 'ship': 1, 'miss': 2, 'hit': 3}
-RENDER_MAPPING = {0: '.', 1: '.', 2: 'M', 3: 'X'}
-OBSERVATION_MAPPING = {0: 0, 1: 0, 2: 2, 3: 3}
-OBSERVATION_DICT = {"unknown": 0, 'miss': 2, 'hit': 3}
 
 
 class GuessResult(Enum):
@@ -19,8 +13,24 @@ class GuessResult(Enum):
     FREE_SHIP_SUNK = 5
     INVALID_GUESS = 6
     GAME_OVER = 7
+    GAME_LOST = 8
+    GAME_WON = 9
 
 
+CELL_MAPPING = {'empty': 0, 'ship': 1, 'miss': 2, 'hit': 3}
+RENDER_MAPPING = {0: '.', 1: '.', 2: 'M', 3: 'X'}
+OBSERVATION_MAPPING = {0: 0, 1: 0, 2: 2, 3: 3}
+OBSERVATION_MAPPING_LIST = [0, 0, 2, 3]
+OBSERVATION_DICT = {"unknown": 0, 'miss': 2, 'hit': 3}
+DEFAULT_CONFIG = {
+    "INVALID_ACTION_RESULT": GuessResult.INVALID_GUESS,
+    "seed": None,
+    "ALLOW_ADJACENT_PLACEMENT": False,
+    "SIZE1_NUM": 4,
+    "SIZE2_NUM": 3,
+    "SIZE3_NUM": 2,
+    "SIZE4_NUM": 1
+}
 REWARD_MAPPING = {
     GuessResult.NO_HIT: 0,
     GuessResult.SHIP_HIT: 0,
@@ -28,31 +38,20 @@ REWARD_MAPPING = {
     GuessResult.SHIP_SUNK: 0,
     GuessResult.FREE_SHIP_SUNK: 0.5,
     GuessResult.INVALID_GUESS: -1.0,
-    GuessResult.GAME_OVER: 0.5
+    GuessResult.GAME_OVER: 0.5,
+    GuessResult.GAME_LOST: -1,
+    GuessResult.GAME_WON: 0.5,
 }
 
 
 class Observation:
+
     def __init__(self, board: np.ndarray):
-        self.board = board
-        self.board[self.board == CELL_MAPPING['empty']] = OBSERVATION_MAPPING[CELL_MAPPING['empty']]
-        self.board[self.board == CELL_MAPPING['ship']] = OBSERVATION_MAPPING[CELL_MAPPING['ship']]
-        self.board[self.board == CELL_MAPPING['miss']] = OBSERVATION_MAPPING[CELL_MAPPING['miss']]
-        self.board[self.board == CELL_MAPPING['hit']] = OBSERVATION_MAPPING[CELL_MAPPING['hit']]
+        self.board = np.array(OBSERVATION_MAPPING_LIST)[board]
 
     def get_one_hot_encoding(self):
-        one_hot_board = np.zeros((*self.board.shape, 3))
-
-        for i in range(one_hot_board.shape[0]):
-            for j in range(one_hot_board.shape[1]):
-                square = self.board[i, j]
-                if square == OBSERVATION_DICT['unknown']:
-                    one_hot_board[i, j, 0] = 1
-                elif square == OBSERVATION_DICT['miss']:
-                    one_hot_board[i, j, 1] = 1
-                elif square == OBSERVATION_DICT['hit']:
-                    one_hot_board[i, j, 2] = 1
-
+        num_classes = 3  # Number of different cell values
+        one_hot_board = np.eye(num_classes)[self.board]
         return one_hot_board
 
     def get_board(self):
@@ -61,7 +60,7 @@ class Observation:
     def get_random_valid_action(self):
         """
             Get a random valid action.
-            
+
             Returns:
             int: A random valid action."""
         valid_actions = np.where(self.board.flatten() == OBSERVATION_DICT['unknown'])
@@ -69,7 +68,8 @@ class Observation:
 
 
 class SeaBattleEnv(gym.Env):
-    def __init__(self, board_shape=(7, 7), reward_func=REWARD_MAPPING, placement: List[Dict[str, Any]] = None):
+    def __init__(self, board_shape=(6, 6), reward_func=REWARD_MAPPING, placement: List[Dict[str, Any]] = None,
+                 config: Dict = DEFAULT_CONFIG):
         super(SeaBattleEnv, self).__init__()
         self.last_guess_correct = None
         self.ships = None
@@ -79,23 +79,21 @@ class SeaBattleEnv(gym.Env):
         self.tot_guesses = None
         self.step_count = None
         self.shape = board_shape
-        self.reset(placement)
+        self.config = config
         self.reward_function = reward_func
+
+        # Set the seed for the environment
+        if self.config['seed'] is not None:
+            npr.seed(self.config['seed'])
+
+        self.reset(placement)
 
     def observation(self):
         return Observation(self.board.copy())
 
     def step(self, action: int):
-        """advance the game
+        """Advance the game by performing the given action."""
 
-        Args:
-            action (int): the number of square to guess. 
-            0   1   2   3   4   5   6   7
-            8   9   10  11  12  13  14  15
-            .
-            .
-            .
-        """
         row, col = divmod(action, self.board.shape[1])
         result = self.attack_square(row, col)
 
@@ -105,49 +103,55 @@ class SeaBattleEnv(gym.Env):
             if result != GuessResult.FREE_SHIP_SUNK and result != GuessResult.FREE_HIT and result != GuessResult.GAME_OVER:
                 self.effective_guesses += 1
 
-        info = {
+        extra_info = {
             'ships': np.count_nonzero(self.board == CELL_MAPPING['ship'])
         }
         self.tot_reward += self.reward_function[result]
-        return self.observation(), self.reward_function[result], result == GuessResult.GAME_OVER, info
+        is_done = result in [GuessResult.GAME_OVER, GuessResult.GAME_LOST, GuessResult.GAME_WON]
+        return self.observation(), self.reward_function[result], is_done, extra_info
 
-    def reset(self, placement: List[Dict[str, Any]]):
+    def reset(self, placement: List[Dict[str, Any]]=None):
+        """Reset the environment to its initial state."""
+
         self.step_count = 0
         self.tot_guesses = 0
         self.effective_guesses = 0
         self.tot_reward = 0
         self.board: np.ndarray = np.full(self.shape, CELL_MAPPING['empty'])
         self.ships: List[Dict[str, Any]] = [
-            {"size": 4, "location": [], "hits": 0},
-            {"size": 3, "location": [], "hits": 0},
-            {"size": 2, "location": [], "hits": 0},
-            {"size": 1, "location": [], "hits": 0}
+            {"size": size, "location": [], "hits": 0}
+            for size in range(4, 0, -1)
+            for _ in range(self.config[f"SIZE{size}_NUM"])
         ]
         self.last_guess_correct = False
         if placement is None:
             self.place_ships_randomly()
         else:
-            ...
+            self.load_ship_placement(placement)
         return self.observation()
 
     def render(self):
+        """Render the current state of the game."""
+
         print("  " + " ".join(str(i) for i in range(self.board.shape[1])))
         for i, row in enumerate(self.observation().get_board()):
             print(str(i) + " " + " ".join(RENDER_MAPPING[cell] for cell in row))
 
     def close(self):
+        """Close the environment."""
+
         pass
 
     def is_valid_location(self, row: int, col: int, ship_size: int, orientation: str):
         """
         Check if the given row, col, and orientation is a valid location for the ship.
-        
+
         Args:
         row (int): The starting row of the ship.
         col (int): The starting column of the ship.
         ship_size (int): The size of the ship.
         orientation (str): The orientation of the ship ('horizontal' or 'vertical').
-        
+
         Returns:
         bool: True if the location is valid, False otherwise.
         """
@@ -157,27 +161,31 @@ class SeaBattleEnv(gym.Env):
             for i in range(col, col + ship_size):
                 if not self.board[row, i] == CELL_MAPPING['empty']:
                     return False
-        else:
+                if not self.config['ALLOW_ADJACENT_PLACEMENT']:
+                    for dr in [-1, 0, 1]:
+                        for dc in [-1, 1]:  # Only check left and right squares
+                            r, c = row + dr, i + dc
+                            if 0 <= r < self.board.shape[0] and 0 <= c < self.board.shape[1] and self.board[r, c] == \
+                                    CELL_MAPPING['ship']:
+                                return False
+        else:  # 'vertical'
             if row + ship_size > self.board.shape[0]:
                 return False
             for i in range(row, row + ship_size):
                 if not self.board[i, col] == CELL_MAPPING['empty']:
                     return False
+                if not self.config['ALLOW_ADJACENT_PLACEMENT']:
+                    for dr in [-1, 1]:  # Only check top and bottom squares
+                        for dc in [-1, 0, 1]:
+                            r, c = i + dr, col + dc
+                            if 0 <= r < self.board.shape[0] and 0 <= c < self.board.shape[1] and self.board[r, c] == \
+                                    CELL_MAPPING['ship']:
+                                return False
         return True
 
     def place_ship(self, row: int, col: int, ship_size: int, orientation: str):
-        """
-        Place the ship on the board with the given row, col, ship_size, and orientation.
-        
-        Args:
-        row (int): The starting row of the ship.
-        col (int): The starting column of the ship.
-        ship_size (int): The size of the ship.
-        orientation (str): The orientation of the ship ('horizontal' or 'vertical').
-        
-        Returns:
-        list: A list of tuples representing the ship's location.
-        """
+        """Place the ship on the board at the given location and orientation."""
+
         ship_location: List[Tuple[int, int]] = []
         if orientation == 'horizontal':
             for i in range(col, col + ship_size):
@@ -191,11 +199,13 @@ class SeaBattleEnv(gym.Env):
 
     def place_ships_randomly(self):
         """Place ships randomly on the board."""
+
         for ship in self.ships:
             placed = False
             while not placed:
-                row = npr.randint(0, self.board.shape[0])
-                col = npr.randint(0, self.board.shape[1])
+                row = npr.randint(0, self.board.shape[0] - 1)
+                col = npr.randint(0, self.board.shape[1] - 1)
+
                 orientation = npr.choice(['horizontal', 'vertical'])
 
                 if self.is_valid_location(row, col, ship["size"], orientation):
@@ -203,45 +213,28 @@ class SeaBattleEnv(gym.Env):
                     placed = True
 
     def load_ship_placement(self, placement: List[Dict[str, Any]]):
-        """
-        Load the ship placement onto the board.
+        """Load the ship placement onto the board."""
 
-        Args:
-        placement (List[Dict[str, Any]]): A list of ship placements. Each ship placement is a dictionary with keys 'size', 'location', and 'hits'.
-        """
         self.ships = placement
-
-        # Clear the board
         self.board = np.full(self.shape, CELL_MAPPING['empty'])
-
-        # Place the ships on the board
         for ship in self.ships:
             for location in ship['location']:
                 row, col = location
                 self.board[row, col] = CELL_MAPPING['ship']
 
     def is_game_over(self) -> bool:
+        """Check if the game is over."""
+
         return all(ship["hits"] == len(ship["location"]) for ship in self.ships)
 
     def is_ship_sunk(self, ship):
-        """
-        Check if the given ship is sunk.
+        """Check if the given ship is sunk."""
 
-        Args:
-        ship (dict): The ship to check, represented as a dictionary with keys 'size', 'location', and 'hits'.
-
-        Returns:
-        bool: True if the ship is sunk, False otherwise.
-        """
         return ship["hits"] == ship["size"]
 
     def reveal_surrounding_squares(self, ship: dict):
-        """
-    Reveal the surrounding squares of a sunken ship on the board.
-    b
-    Args:
-    ship (dict): The sunken ship, represented as a dictionary with keys 'size', 'location', and 'hits'.
-    """
+        """Reveal the surrounding squares of a sunken ship on the board."""
+
         for row, col in ship["location"]:
             for dr in range(-1, 2):
                 for dc in range(-1, 2):
@@ -251,18 +244,10 @@ class SeaBattleEnv(gym.Env):
                         self.board[r, c] = CELL_MAPPING['miss']
 
     def attack_square(self, row: int, col: int):
-        """
-            Attack the square at the given row and column.
+        """Attack the square at the given row and column."""
 
-            Args:
-            row (int): The row of the square to attack.
-            col (int): The column of the square to attack.
-
-            Returns:
-            GuessResult: An Enum member representing the result of the attack: NO_HIT, SHIP_HIT, FREE_HIT, SHIP_SUNK, FREE_SHIP_SUNK, INVALID_GUESS,  GAME_OVER.
-        """
         if row < 0 or row >= self.board.shape[0] or col < 0 or col >= self.board.shape[1]:
-            return GuessResult.INVALID_GUESS
+            return self.config["INVALID_ACTION_RESULT"]
 
         if self.board[row, col] == CELL_MAPPING['ship']:
             self.board[row, col] = CELL_MAPPING['hit']
@@ -271,7 +256,6 @@ class SeaBattleEnv(gym.Env):
                     ship["hits"] += 1
                     if self.last_guess_correct:
                         guess_result = GuessResult.FREE_HIT
-
                     else:
                         guess_result = GuessResult.SHIP_HIT
                         self.last_guess_correct = True
@@ -289,44 +273,44 @@ class SeaBattleEnv(gym.Env):
             self.last_guess_correct = False
             return GuessResult.NO_HIT
         else:
-            return GuessResult.INVALID_GUESS
+            return self.config["INVALID_ACTION_RESULT"]
 
     def __str__(self):
+        """Convert the current state of the game to a string."""
+
         board_str = "  " + " ".join(str(i) for i in range(self.board.shape[1])) + "\n"
         for i, row in enumerate(self.board):
             board_str += str(i) + " " + " ".join(RENDER_MAPPING[cell] for cell in row) + "\n"
         return board_str
 
     def get_stats(self):
-        """return the current stats of the env
+        """Get the current stats of the game."""
 
-        Returns:
-            tuple: (step_count, total num of guesses, num of effective guesses, total reward)
-        """
         return self.step_count, self.tot_guesses, self.effective_guesses, self.tot_reward
 
 
 if __name__ == "__main__":
-    shape = (4, 4)
+    shape = (10, 10)
     env = SeaBattleEnv(shape)
-    for step in range(200):
-        env.render()
-        action = int(input("Enter action:"))
+    for step in range(100000):
+        # env.render()
         action = env.observation().get_random_valid_action()
+        # aaction = int(input("enter action"))
         obs, reward, done, info = env.step(action)
 
-        print("----------result:------------")
-        print("obs:")
-        print(obs.get_board())
-        print("obs_one_hot:")
-        print(obs.get_one_hot_encoding().flatten())
-        step, tot_guesses, effective_guesses = env.get_stats()
-        print("stats:")
-        print(f"reward: {reward}")
-        print(f"step: {step}")
-        print(f"tot_guesses: {tot_guesses}")
-        print(f"effective_guesses: {effective_guesses}")
-        print("---------------------------------")
+        # print("----------result:------------")
+        # print("obs:")
+        # print(obs.get_board())
+        # print("obs_one_hot:")
+        # print(obs.get_one_hot_encoding().flatten())
+        # step, tot_guesses, effective_guesses, tot_reward = env.get_stats()
+        # print("stats:")
+        # print(f"reward: {reward}")
+        # print(f"step: {step}")
+        # print(f"tot_guesses: {tot_guesses}")
+        # print(f"effective_guesses: {effective_guesses}")
+        # print(f"tot_reward: {tot_reward}")
+        # print("---------------------------------")
 
         if done:
             print("Game over!")
